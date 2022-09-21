0bd1f8147e52ce0b3ced4f112be643d74d6f5d0c
Eleazar Freitas <eleazarsf@gmail.com>
2022-09-21 12:49:37 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 fetch('http://worldtimeapi.org/api/ip/')
-.then(function(response) {
+    .then(function(response) {
         response.json().then(json => {
             const realTime = json.datetime.slice(0, 19);
             const timezone = json.timezone.split("/");
             const city = timezone[timezone.length -1];
+            console.log(realTime)
             const date = new Date(realTime);
             const clockRunning = setInterval(
                 function time () {
@@ -12,6 +13,10 @@ fetch('http://worldtimeapi.org/api/ip/')
                     const secs = date.getSeconds();
                     const fullTime = `${hours} : ${mins} : ${secs}`
                     date.setSeconds(secs + 1);
+                    if (hours < 05 || hours > 18) {
+                        document.querySelector("html").classList.add("dark-mode");
+                        document.querySelector("h1").innerHTML = `<i class="fa-solid fa-moon"></i>`;
+                    }
                     document.querySelector("span").innerText = fullTime;
                     document.querySelector("p").innerText = city;
                 }, 1000
