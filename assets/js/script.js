f0948ff9772fdd3c1e3f6c90adc887635634556f
Eleazar Freitas <eleazarsf@gmail.com>
2022-09-21 12:07:00 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
-const clockRunning = setInterval (
-    function time () {
-        fetch('http://worldtimeapi.org/api/ip/')
-            .then(function(response) {
-                response.json().then(json => {
-                    const realTime = json.datetime;
-                    const onlyTime = realTime.slice(11, -13);
-                    const hour = onlyTime.slice(0, 2);
-                    if (hour < 05 || hour > 18) {
-                        document.querySelector("html").classList.add("dark-mode")
-                        document.querySelector("h1").innerHTML = `<i class="fa-solid fa-moon"></i>`;
-                    }
-                    document.querySelector("span").innerText = onlyTime;
-                });
-            })
-            .catch(function(error) {
-                console.log(error);
-                }
-            );
-    }, 1000
-)
+fetch('http://worldtimeapi.org/api/ip/')
+.then(function(response) {
+        response.json().then(json => {
+            const realTime = json.datetime.slice(0, 19);
+            const timezone = json.timezone.split("/");
+            const city = timezone[timezone.length -1];
+            const date = new Date(realTime);
+            const clockRunning = setInterval(
+                function time () {
+                    const hours = date.getHours();
+                    const mins = date.getMinutes();
+                    const secs = date.getSeconds();
+                    const fullTime = `${hours} : ${mins} : ${secs}`
+                    date.setSeconds(secs + 1);
+                    document.querySelector("span").innerText = fullTime;
+                    document.querySelector("p").innerText = city;
+                }, 1000
+                )
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        }
+    );
