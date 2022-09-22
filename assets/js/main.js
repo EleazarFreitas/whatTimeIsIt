c808da6286424fd25c3b47e97f1a1bf1010096db
EleazarSF <eleazarsf@gmail.com>
2022-09-22 17:03:54 -0300

    clock 90% completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,11 @@ fetch('http://worldtimeapi.org/api/ip/')
 
                 const options = {
                     year: 'numeric',
-                    month: 'short',
+                    month: 'long',
                     day: 'numeric',
                   };
 
-                let fullDate = date.toLocaleString("pt-BR", options);
+                let fullDate = `<i class="fa-regular fa-calendar"></i> ` + date.toLocaleString("en-US", options);
     
                 let hours = date.getHours();
                 let mins = date.getMinutes();
@@ -31,30 +31,32 @@ fetch('http://worldtimeapi.org/api/ip/')
                 const fullTime = `${hours}:${mins}<span class="display-6">:${secs}</span>`;
                 date.setSeconds(secs + 1);
 
-                const greetings = document.querySelector(".greetings");
                 const body = document.querySelector("body");
+                const greetings = document.querySelector(".greetings");
+                const am_pm = document.querySelector(".am-pm");
 
-                if (hours > 4) {
-                    body.classList.add("morning");
-                    greetings.innerHTML = `<i class="fa-regular fa-sun"></i> BOM DIA!`;
-                } 
-                if (hours > 11) {
-                    body.classList.add("afternoon");
-                    greetings.innerHTML = `<i class="fa-regular fa-sun"></i> BOA TARDE!`;
-                    document.querySelector(".am-pm").innerHTML = `PM`;
-                }
                 if (hours > 18) {
                     body.classList.add("night");
-                    greetings.innerHTML = `<i class="fa-solid fa-moon"></i> BOA NOITE!`;
-                }
-                if (hours >= 0 && hours < 5) {
+                    greetings.innerHTML = `<i class="fa-solid fa-moon"></i> GOOD EVENING!`;
+                    am_pm.innerHTML = `PM`;
+                } else if (hours > 11) {
+                    body.classList.add("afternoon");
+                    greetings.innerHTML = `<i class="fa-regular fa-sun"></i> GOOD AFTERNOON!`;
+                    am_pm.innerHTML = `PM`;
+                } else if (hours > 4) {
+                    body.classList.add("morning");
+                    greetings.innerHTML = `<i class="fa-solid fa-moon"></i> GOOD MORNING!`;
+                    am_pm.innerHTML = `AM`;
+                } else {
                     body.classList.add("night");
-                    greetings.innerHTML = `<i class="fa-solid fa-moon"></i> BOA MADRUGADA!`;
+                    greetings.innerHTML = `<i class="fa-solid fa-moon"></i> GOOD EVENING!`;
+                    am_pm.innerHTML = `AM`;
                 }
+                
 
                 document.querySelector("h1").innerHTML = fullTime;
                 document.querySelector(".date").innerHTML = fullDate;
-                document.querySelector(".city").innerHTML = city;
+                document.querySelector(".city").innerHTML = `<i class="fa-solid fa-location-dot"></i> ` + city;
 
             }, 1000
             )
