597e82616ea362ef74f05848de503d4b4bfff51e
EleazarSF <eleazarsf@gmail.com>
2022-09-22 08:46:42 -0300

    trying to solve random times

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// import getLocalTime from "./modules/getLocalTime.js";
+import getRandomTimeAroundTheWorld from "./modules/getRandomTimeAroundTheWorld.js";
+
+// getLocalTime();
+getRandomTimeAroundTheWorld();
